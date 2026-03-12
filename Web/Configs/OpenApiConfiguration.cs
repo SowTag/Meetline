94e6648d5752cfe32775aca63b9ab94a4a0093af
Maddock <sowtag@gmail.com>
2026-03-12 20:25:41 -0300

    feat: add Scalar as an OpenAPI testing client

---
+++ Web/Configs/OpenApiConfiguration.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace Web.Configs;
+
+public static class OpenApiConfiguration
+{
+    public static void Configure(OpenApiOptions options)
+    {
+        options.AddDocumentTransformer((document, context, cancellationToken) =>
+        {
+            document.Info.Title = "Meetline";
+            document.Info.Version = "v0.0.1";
+            document.Info.Description = "Meetline is an online learning and conferencing app.";
+
+            document.Info.Contact = new OpenApiContact
+            {
+                Name = "Maddock",
+                Email = "sowtag@gmail.com",
+                Url = new Uri("https://github.com/SowTag")
+            };
+
+            return Task.CompletedTask;
+        });
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,7 +1,16 @@
+using Scalar.AspNetCore;
+using Web.Configs;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(OpenApiConfiguration.Configure);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()) app.MapOpenApi();
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.Run();
