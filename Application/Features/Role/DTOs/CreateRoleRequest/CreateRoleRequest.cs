1b5b44ddf9989d3a5d7480e6ae36c8b4d62ccef6
Maddock <sowtag@gmail.com>
2026-04-05 06:08:26 -0300

    fix: make CreateRoleRequest.cs fields required

--- Application/Features/Role/DTOs/CreateRoleRequest/CreateRoleRequest.cs
+++ Application/Features/Role/DTOs/CreateRoleRequest/CreateRoleRequest.cs
@@ -2,4 +2,10 @@ using Domain.Wrappers;
 
 namespace Application.Features.Role.DTOs.CreateRoleRequest;
 
-public record CreateRoleRequest(string Name, bool Hoist, int Position, PermissionSet Permissions);
+public record CreateRoleRequest
+{
+    public required string Name { get; init; }
+    public required bool Hoist { get; init; }
+    public required int Position { get; init; }
+    public required PermissionSet Permissions { get; init; }
+}
