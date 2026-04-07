e43f1bc83c21cb8f44c5677648993783800f4026
Maddock <sowtag@gmail.com>
2026-04-06 23:52:40 -0300

    feat: proper loading spinner when loading auth

--- MeetlineUI/src/main.tsx
+++ MeetlineUI/src/main.tsx
@@ -1,6 +1,16 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import { ClerkProvider, useAuth } from '@clerk/react'
+import { env } from '#/env.ts'
+import { Spinner } from '#/components/ui/spinner.tsx'
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '#/components/ui/empty.tsx'
 
 const router = createRouter({
   routeTree,
@@ -18,5 +28,31 @@ const rootElement = document.getElementById('app')!
 
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
-  root.render(<RouterProvider router={router} />)
+  root.render(
+    <ClerkProvider publishableKey={env.VITE_CLERK_PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>,
+  )
+}
+
+function App() {
+  const auth = useAuth()
+
+  if (!auth.isLoaded) return <AuthLoadingSpinner />
+
+  return <RouterProvider router={router} context={{ auth }} />
+}
+
+function AuthLoadingSpinner() {
+  return <main className={'fixed inset-0 flex justify-center'}>
+    <Empty>
+      <EmptyHeader>
+        <EmptyMedia variant={'icon'}>
+          <Spinner />
+        </EmptyMedia>
+        <EmptyTitle>Meetline</EmptyTitle>
+        <EmptyDescription>Signing you in...</EmptyDescription>
+      </EmptyHeader>
+    </Empty>
+  </main>
 }
