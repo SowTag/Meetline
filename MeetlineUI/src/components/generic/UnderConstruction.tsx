ab0dbc41a236de57e651ebcaa133a6cb8d620597
Maddock <sowtag@gmail.com>
2026-04-07 16:31:56 -0300

    feat: add cool under construction component and add it to /calendar

---
+++ MeetlineUI/src/components/generic/UnderConstruction.tsx
@@ -0,0 +1,21 @@
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '#/components/ui/empty.tsx'
+import { ConstructionIcon } from 'lucide-react'
+import type { ComponentProps } from 'react'
+
+export function UnderConstruction(props: ComponentProps<typeof Empty>) {
+  return <Empty {...props}>
+    <EmptyHeader>
+      <EmptyMedia variant={'icon'}>
+        <ConstructionIcon />
+      </EmptyMedia>
+      <EmptyTitle>Under construction</EmptyTitle>
+      <EmptyDescription>You've reached a planned, but under development part of Meetline. Lucky you!</EmptyDescription>
+    </EmptyHeader>
+  </Empty>
+}
--- MeetlineUI/src/routes/_authenticated/_sidebar/calendar/index.tsx
+++ MeetlineUI/src/routes/_authenticated/_sidebar/calendar/index.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { UnderConstruction } from '#/components/generic/UnderConstruction.tsx'
 
 export const Route = createFileRoute('/_authenticated/_sidebar/calendar/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authenticated/_sidebar/calendar/"!</div>
+  return (
+    <main className={'flex flex-col items-center justify-center h-full'}>
+      <UnderConstruction />
+    </main>
+  )
 }
