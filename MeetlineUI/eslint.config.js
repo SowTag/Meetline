2339ad505fb22e01ed2a3b7d1941ace48f059063
Maddock <sowtag@gmail.com>
2026-04-27 04:27:37 -0300

    feat: set up Storybook in the project

--- MeetlineUI/eslint.config.js
+++ MeetlineUI/eslint.config.js
@@ -1,23 +1,22 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [
-  ...tanstackConfig,
-  {
-    rules: {
-      'import/no-cycle': 'off',
-      '@typescript-eslint/array-type': 'off',
-      '@typescript-eslint/require-await': 'off',
-      'pnpm/json-enforce-catalog': 'off',
-    },
+export default [...tanstackConfig, {
+  rules: {
+    'import/no-cycle': 'off',
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/require-await': 'off',
+    'pnpm/json-enforce-catalog': 'off',
   },
-  {
-    ignores: [
-      'eslint.config.js',
-      'prettier.config.js',
-      'src/components/ui/**',
-      'src/client',
-    ],
-  },
-]
+}, {
+  ignores: [
+    'eslint.config.js',
+    'prettier.config.js',
+    'src/components/ui/**',
+    'src/client',
+  ],
+}, ...storybook.configs["flat/recommended"]];
--- MeetlineUI/vite.config.ts
+++ MeetlineUI/vite.config.ts
@@ -1,34 +1,60 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import { devtools } from '@tanstack/devtools-vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const config = defineConfig({
   server: {
     allowedHosts: ['meetline.maddock.world'],
-    proxy: { '/api': 'http://localhost:5200' },
+    proxy: {
+      '/api': 'http://localhost:5200'
+    }
   },
-  plugins: [
-    devtools(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
-    tailwindcss(),
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
-    viteReact({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
-  ],
+  plugins: [devtools(), tsconfigPaths({
+    projects: ['./tsconfig.json']
+  }), tailwindcss(), tanstackRouter({
+    target: 'react',
+    autoCodeSplitting: true
+  }), viteReact({
+    babel: {
+      plugins: ['babel-plugin-react-compiler']
+    }
+  })],
   build: {
-    chunkSizeWarningLimit: 800, // kB
+    chunkSizeWarningLimit: 800 // kB
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
-})
-
-export default config
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: 'playwright',
+          instances: [{
+            browser: 'chromium'
+          }]
+        }
+      }
+    }]
+  }
+});
+export default config;
