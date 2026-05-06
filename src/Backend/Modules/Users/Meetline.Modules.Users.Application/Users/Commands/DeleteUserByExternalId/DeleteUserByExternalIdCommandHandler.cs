e46fcb791eeabef3becb1abbdbb9c7547d8981ef
Maddock <sowtag@gmail.com>
2026-05-06 13:53:55 -0300

    fix: fix DeleteUserByExternalIdCommandHandler

--- src/Backend/Modules/Users/Meetline.Modules.Users.Application/Users/Commands/DeleteUserByExternalId/DeleteUserByExternalIdCommandHandler.cs
+++ src/Backend/Modules/Users/Meetline.Modules.Users.Application/Users/Commands/DeleteUserByExternalId/DeleteUserByExternalIdCommandHandler.cs
@@ -1,20 +1,23 @@
 using FluentResults;
 using Mediator;
 using Meetline.Modules.Users.Application.Users.Errors;
+using Meetline.Modules.Users.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace Meetline.Modules.Users.Application.Users.Commands.DeleteUserByExternalId;
 
-public class DeleteUserByExternalIdCommandHandler(IUserRepository repository)
+internal class DeleteUserByExternalIdCommandHandler(UsersDbContext context)
     : ICommandHandler<DeleteUserByExternalIdCommand, Result>
 {
     public async ValueTask<Result> Handle(DeleteUserByExternalIdCommand byExternalIdCommand,
         CancellationToken cancellationToken)
     {
-        var user = await repository.GetUserByExternalId(byExternalIdCommand.ExternalId, cancellationToken);
+        var user = await context.Users
+            .FirstOrDefaultAsync(u => u.ExternalId == byExternalIdCommand.ExternalId, cancellationToken);
 
         if (user is null) return Result.Fail(new UserNotFoundError(byExternalIdCommand.ExternalId));
 
-        await repository.DeleteUser(user, cancellationToken);
+        context.Users.Remove(user);
 
         return Result.Ok();
     }
