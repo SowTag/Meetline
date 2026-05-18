35432ccf2a598bad96505b48b566801480cc0fb0
Maddock <sowtag@gmail.com>
2026-05-18 13:25:15 -0300

    refactor: make user sync data mapper static

--- src/Backend/Modules/Users/Meetline.Modules.Users.Application/Services/UserSyncDataMapper.cs
+++ src/Backend/Modules/Users/Meetline.Modules.Users.Application/Services/UserSyncDataMapper.cs
@@ -4,10 +4,10 @@ using Riok.Mapperly.Abstractions;
 namespace Meetline.Modules.Users.Application.Services;
 
 [Mapper]
-public partial class UserSyncDataMapper
+public static partial class UserSyncDataMapper
 {
     [MapperIgnoreTarget(nameof(User.Id))]
     [MapperIgnoreTarget(nameof(User.CreatedAt))]
     [MapperIgnoreTarget(nameof(User.UpdatedAt))]
-    public partial User ToUser(UserSyncData idpUserSyncData);
+    public static partial User ToUser(UserSyncData idpUserSyncData);
 }
