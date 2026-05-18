e68c4dac961cd562dfbf0aa617b6822601813130
Maddock <sowtag@gmail.com>
2026-05-18 13:04:48 -0300

    refactor: improve GetUserIdByExternalIdHandler.cs

--- src/Backend/Modules/Users/Meetline.Modules.Users.Application/Users/Queries/GetUserIdByExternalId/GetUserIdByExternalIdHandler.cs
+++ src/Backend/Modules/Users/Meetline.Modules.Users.Application/Users/Queries/GetUserIdByExternalId/GetUserIdByExternalIdHandler.cs
@@ -1,24 +1,18 @@
-using FluentResults;
 using Meetline.Modules.Users.Application.Data;
-using Meetline.Modules.Users.Application.Users.DTOs.UserGuidResponse;
-using Meetline.Modules.Users.Application.Users.Errors;
 using Microsoft.EntityFrameworkCore;
 
 namespace Meetline.Modules.Users.Application.Users.Queries.GetUserIdByExternalId;
 
 public static class GetUserIdByExternalIdQueryHandler
 {
-    public static async ValueTask<Result<UserGuidResponse>> Handle(GetUserIdByExternalIdQuery query,
-        IUsersDbContext dbContext,
+    public static Task<Guid?> Handle(GetUserIdByExternalIdQuery query,
+        IUsersDbContext context,
         CancellationToken cancellationToken)
     {
-        var id = await dbContext.Users
+        return context.Users
+            .AsNoTracking()
             .Where(u => u.ExternalId == query.ExternalId)
             .Select(u => (Guid?)u.Id)
             .FirstOrDefaultAsync(cancellationToken);
-
-        return id is null
-            ? Result.Fail(new UserNotFoundError(query.ExternalId))
-            : Result.Ok(new UserGuidResponse(id.Value));
     }
 }
