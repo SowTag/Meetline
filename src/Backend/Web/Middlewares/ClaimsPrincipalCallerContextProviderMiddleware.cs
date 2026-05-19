d3083cc0a8d10ffd5b53d1faf6a1c32115a5cbe5
Maddock <sowtag@gmail.com>
2026-05-19 01:52:48 -0300

    feat: proper internal id handling in caller context provider middleware

--- src/Backend/Web/Middlewares/ClaimsPrincipalCallerContextProviderMiddleware.cs
+++ src/Backend/Web/Middlewares/ClaimsPrincipalCallerContextProviderMiddleware.cs
@@ -4,23 +4,25 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Web.Scopes;
+using Meetline.Modules.Users.Application.Users.Queries.GetInternalUserId;
+using Wolverine;
 
 namespace Web.Middlewares;
 
 public class ClaimsPrincipalCallerContextProviderMiddleware
 {
-    public static ICallerContext Before(IHttpContextAccessor accessor)
+    public static async Task<ICallerContext> Before(IHttpContextAccessor accessor, IMessageBus bus)
     {
         var context = accessor.HttpContext;
 
         if (context is null) throw new UnauthorizedAccessException();
 
         var externalId = context.User.FindFirst(ClaimTypes.NameIdentifier);
-        
+
         if (externalId is null) throw new UnauthorizedAccessException();
-        
-        // TODO synchronous sync with IdP
-        
-        return new WebProvidedCallerContext(Guid.NewGuid(), externalId.Value);
+
+        var internalId = await bus.InvokeAsync<Guid>(new GetInternalUserIdQuery(externalId.Value));
+
+        return new WebProvidedCallerContext(internalId, externalId.Value);
     }
 }
